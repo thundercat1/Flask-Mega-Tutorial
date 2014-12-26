7ca3eff6e13d33f4c6281574bb6fe62b3ae65f20
Thundercat <michael.c.haines@gmail.com>
2014-12-26 15:25:29 -0700

    Form with redirect and flashing

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 import flask
+import forms
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,13 @@ def index():
             }
         ]
     return flask.render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        flask.flash('Login requested for "%s". Remember me: "%s"' %
+                (form.openid.data, str(form.remember_me.data)))
+        return flask.redirect('/index')
+
+    return flask.render_template('login.html', title='login', form=form)
---
+++ config.py
@@ -0,0 +1,2 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'You will never guess the key'
