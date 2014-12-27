1903cb8fd845d3f6170e11dd1660f33da38fa702
mch chromebook <michael.c.haines@gmail.com>
2014-12-27 11:36:40 -0700

    Database complete on Chromebook

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
 from app import db
 
-class user(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	nickname = db.Column(db.String(64), unique=True, index=True)
-	email = db.Column(db.String(120), index=True, unique=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
 
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-	def __repr__(self):
-		return '<User %s>' % (self.nickname)
+    def __repr__(self):
+        return '<User %s>' % (self.nickname)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %s>' % (self.body)
