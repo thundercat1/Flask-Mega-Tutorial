f08db77ba37f884dbfe30001e00ed1a4c6bf2e70
mch chromebook <michael.c.haines@gmail.com>
2014-12-28 13:32:02 -0700

    Authorization with OpenID

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import base_path
+
+import os
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(base_path, 'tmp'))
 
 from app import views, models
